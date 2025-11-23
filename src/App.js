ba3a6ab2137bedb3191229a62efe467b0e7932b9
52330918-cloud <52330918@students.liu.edu.lb>
2025-11-23 20:48:46 +0200

    preparing routes in app including the login-logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Courses from "./components/Courses";
+import Login from "./components/Login";
 import About from "./components/About";
+import Home from "./components/Home";
+import Registration from "./components/Registration";
+import ContactUs from "./components/ContactUs";
+import { useState } from "react";
 
 function App() {
-  return (
+   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
     <div>
-     <></>
+   
 
     </div>
+  return (
+    <div>
+    {isLoggedIn && <Navbar onLogout={handleLogout} />}
+ <Routes>
+      <Route path="/" Component={() => <Login onLoginSuccess={handleLogin} />} />
+      <Route path="/home" Component={Home} />
+      <Route path="/about" Component={About} />
+      <Route path="/registration" Component={Registration} />
+      <Route path="/contact" Component={ContactUs} />
+
+          
+        
+
+     </Routes> </div>
   );
 }
 
