f9585e31aa5b3c718263f316539c8e4d065cb729
52330918-cloud <52330918@students.liu.edu.lb>
2025-11-23 20:59:22 +0200

    putting the link components in the right of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 function Navbar({onLogout}) {
@@ -16,7 +16,38 @@ function Navbar({onLogout}) {
         <span className="text-xl font-semibold text-white  drop-shadow">Student Portal</span>
       </div>
 
-
+ <div className="flex gap-6 items-center">
+        <Link
+          className="text-white font-medium px-4 py-2 rounded-lg hover:bg-blue-600 hover:text-amber-200 transition duration-200 "
+          to="/home"
+        >
+          Home
+        </Link>
+        <Link
+          className="text-white font-medium px-4 py-2 rounded-lg hover:bg-blue-600 hover:text-amber-200 transition duration-200 "
+          to="/about"
+        >
+          About
+        </Link>
+        <Link
+          className="text-white font-medium px-4 py-2 rounded-lg hover:bg-blue-600 hover:text-amber-200 transition duration-200 "
+          to="/registration"
+        >
+          Registration
+        </Link>
+        <Link
+          className="text-white font-medium px-4 py-2 rounded-lg hover:bg-blue-600 hover:text-amber-200 transition duration-200 "
+          to="/contact"
+        >
+          Contact
+        </Link>
+        <button
+          onClick={handleLogout}
+          className="text-white font-medium px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 transition duration-200 "
+        >
+          Logout
+        </button>
+      </div>
 
 
 
