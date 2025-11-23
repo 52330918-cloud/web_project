42d5a503a4ff31cb84a7df1b39e1ba82adf08ee5
52330918-cloud <52330918@students.liu.edu.lb>
2025-11-23 20:30:19 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Courses from "./components/Courses";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div>
+     <></>
+
+    </div>
+  );
+}
+
+export default App;
